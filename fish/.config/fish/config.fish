a670a4e5b88741a55ece6f71333a73cdc8d45e93
Nelu Snegur <nelu.snegur@pulsarplanet.com>
2026-04-18 18:26:01 +0200

    Display username instead of hostname

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -41,7 +41,7 @@ function fish_prompt
     set_color brblack
     echo -n "["(date "+%H:%M")"] "
     set_color blue
-    echo -n (hostname)
+    echo -n $USER
 
     if [ $PWD != $HOME ]
       set_color brblack
